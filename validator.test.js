5678ff66ac419658cd849c342617f9aa1c578b14
crazylogic03 <saithrishul2006@gmail.com>
2026-04-16 10:07:31 +0530

    Test for Validator File

--- validator.test.js
+++ validator.test.js
@@ -6,3 +6,18 @@ test(("valid email"),()=>{
     expect(isValidEmail("")).toBe(false)
 })
 
+test(("strong password"),()=>{
+    expect(isStrongPassword("")).toBe(false)
+    expect(isStrongPassword("12345678")).toBe(false)
+    expect(isStrongPassword("abcdefgh")).toBe(false)
+    expect(isStrongPassword("ABCDEFGH")).toBe(false)
+    expect(isStrongPassword("Abcdefgh")).toBe(false)
+    expect(isStrongPassword("Abcdefg1")).toBe(true)
+})
+
+test(("non empty string"),()=>{
+    expect(isNonEmptyString("   ")).toBe(false)
+    expect(isNonEmptyString("               ")).toBe(false)
+    expect(isNonEmptyString("")).toBe(false)
+    expect(isNonEmptyString("Hello")).toBe(true)
+})  
